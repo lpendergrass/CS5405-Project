5b1e956a6f0df4807f6f51446586d34a3a6cad3e
lpendergrass <lpgy9@mst.edu>
2014-12-02 02:35:53 -0600

    Added some basic documentation

--- graphPane.java
+++ graphPane.java
@@ -10,9 +10,11 @@ import javax.swing.border.EtchedBorder;
 
 public class graphPane extends JPanel {
 
-	// Array Parameters
+	// Initial Array Parameters
 	int arrayLen = 20;
 	int maxVal = 100;
+	
+	// Random number generator
 	Random rand = new Random();
 	
 	ArrayList<Integer> array;
@@ -20,13 +22,17 @@ public class graphPane extends JPanel {
 	public graphPane(){
 		super();
 
+		// Set border around graph
 		setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
 		
+		// Instantiate ArrayList
 		array = new ArrayList<Integer>();
 		
+		// Generate initial array values
 		populate( arrayLen, maxVal );
 	}
 	
+	// Clears and populates the array with random integers
 	public void populate( int len, int max ) {
 		
 		arrayLen = len;
@@ -38,19 +44,23 @@ public class graphPane extends JPanel {
 	    	array.add(rand.nextInt(maxVal));
 	}
 	
+	// Swaps items at the specified indices
 	public void swap( int x, int y ){
 		Collections.swap(array, x, y);
 		repaint();
 	}
 	
+	// Returns the array value at the specified index
 	public int get( int x ){ 
 		return array.get(x);
 	}
 	
+	// Returns the length of the array
 	public int length(){
 		return arrayLen;
 	}
 	
+	// Paints the arrays values as a bar graph
 	public void paint(Graphics g){
 		super.paint(g);
 		
@@ -60,10 +70,14 @@ public class graphPane extends JPanel {
 		
 		for( int i = 0; i < arrayLen; i++ ){
 			
+			// Scale bar i's height
 			barHeight = (int)(array.get(i) * heightRatio);
 			
+			// Fill bar i's background
 			g.setColor(Color.green);
 			g.fillRect( i * barWidth, getHeight() - barHeight - 2, barWidth, barHeight);
+			
+			// Draw bar i's border
 			g.setColor(Color.black);
 			g.drawRect( i * barWidth, getHeight() - barHeight - 2, barWidth, barHeight);
 	}
